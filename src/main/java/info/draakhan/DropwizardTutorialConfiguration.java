833d1e2af074d39c0813c7a4c175f8d92a637fff
Bartłomiej Szymański <github.com@draakhan.info>
2016-08-13 18:18:01 +0200

    Added configuration of an application

--- src/main/java/info/draakhan/DropwizardTutorialConfiguration.java
+++ src/main/java/info/draakhan/DropwizardTutorialConfiguration.java
@@ -3,8 +3,32 @@ package info.draakhan;
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
 
 public class DropwizardTutorialConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
 }
